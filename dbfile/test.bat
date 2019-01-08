78acad9ff502dad573c68eefbd3cb62ef1efc3e7
swamyvenkatesh <swamyvenkateshch@gmail.com>
2019-01-08 12:54:49 +0530

    Create test.bat

---
+++ dbfile/test.bat
@@ -0,0 +1,8 @@
+@echo off
+set datecode=%date:~-4%-%date:~4,2%-%date:~7,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+set itr=1
+cd /D C:\HiveCenter_cARM_bots
+echo Trg > C:\Prudential\TriggerFolder\%2.trg
+cmd /c CheckJobs.cmd %1 %2 "%datecode%" %itr%
+echo "Error: "
+exit 1
